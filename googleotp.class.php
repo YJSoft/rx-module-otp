dfb95332a18bfd69dc4cec0beb5411d758c35c07
BJRambo <sosifam@070805.co.kr>
2019-06-10 15:58:50 +0900

    PHP7.2 Object 호환성 문제 고침

--- googleotp.class.php
+++ googleotp.class.php
@@ -2,7 +2,7 @@
 class googleotp extends ModuleObject
 {
 	private $triggers = array(
-		array("moduleHandler.init","googleotp","controller","triggerHijackLogin","before"),
+		array('moduleHandler.init', 'googleotp', 'controller', 'triggerHijackLogin', 'before'),
 		array('moduleHandler.init', 'googleotp', 'controller', 'triggerAddMemberMenu', 'after')
 	);
 
@@ -15,7 +15,7 @@ class googleotp extends ModuleObject
 			$oModuleController->insertTrigger($trigger[0], $trigger[1], $trigger[2], $trigger[3], $trigger[4]);
 		}
 
-		return new Object();
+		return $this->createObject();
 	}
 
 	function checkUpdate()
@@ -46,16 +46,27 @@ class googleotp extends ModuleObject
 			}
 		}
 
-		return new Object(0, 'success_updated');
+		return $this->createObject(0, 'success_updated');
 	}
 
 	function moduleUninstall()
 	{
-		return new Object();
+		return $this->createObject();
 	}
 
 	function recompileCache()
 	{
-		return new Object();
+		return $this->createObject();
+	}
+
+	public function createObject($status = 0, $message = 'success' /* $arg1, $arg2 ... */)
+	{
+		$args = func_get_args();
+		if (count($args) > 2)
+		{
+			global $lang;
+			$message = vsprintf($lang->$message, array_slice($args, 2));
+		}
+		return class_exists('BaseObject') ? new BaseObject($status, $message) : new Object($status, $message);
 	}
 }
