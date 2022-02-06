4b26c29c618dbbce17b11034eb759d08e26dabaf
Waterticket <matthew218@naver.com>
2022-02-06 14:07:23 +0900

    관리자 페이지에서 인증기록과 인증키 발송내역 확인 추가

--- googleotp.admin.view.php
+++ googleotp.admin.view.php
@@ -55,4 +55,50 @@ class googleotpAdminView extends googleotp
 		$config = $this->getConfig();
 		Context::set('googleotp_config', $config);
 	}
+
+	public function dispGoogleotpAdminAuthList()
+	{
+		$oMemberModel = getModel('member');
+		
+		$args = new stdClass;
+		$args->page = Context::get('page'); ///< 페이지
+		$args->list_count = 20; ///< 한페이지에 보여줄 기록 수
+		$args->page_count = 10; ///< 페이지 네비게이션에 나타날 페이지의 수
+		$args->order_type = 'desc';
+		$output = executeQueryArray('googleotp.getGoogleotpauthlogList', $args);
+
+		foreach ($output->data as $key => $datum)
+		{
+			$output->data[$key]->member_info = $oMemberModel->getMemberInfoByMemberSrl($datum->srl);
+		}
+
+		Context::set('total_count', $output->total_count);
+		Context::set('total_page', $output->total_page);
+		Context::set('page', $output->page);
+		Context::set('page_navigation', $output->page_navigation);
+		Context::set('auth_list', $output->data);
+	}
+
+	public function dispGoogleotpAdminAuthSendList()
+	{
+		$oMemberModel = getModel('member');
+		
+		$args = new stdClass;
+		$args->page = Context::get('page'); ///< 페이지
+		$args->list_count = 20; ///< 한페이지에 보여줄 기록 수
+		$args->page_count = 10; ///< 페이지 네비게이션에 나타날 페이지의 수
+		$args->order_type = 'desc';
+		$output = executeQueryArray('googleotp.getAuthSendLogList', $args);
+
+		foreach ($output->data as $key => $datum)
+		{
+			$output->data[$key]->member_info = $oMemberModel->getMemberInfoByMemberSrl($datum->member_srl);
+		}
+
+		Context::set('total_count', $output->total_count);
+		Context::set('total_page', $output->total_page);
+		Context::set('page', $output->page);
+		Context::set('page_navigation', $output->page_navigation);
+		Context::set('authsend_list', $output->data);
+	}
 }
