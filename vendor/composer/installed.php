743fa009d65ba9a3fe001bbf4a10645e4f31bf19
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2026-02-16 11:23:24 +0000

    composer 라이브러리 추가

---
+++ vendor/composer/installed.php
@@ -0,0 +1,225 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '0f39aa9d39525e7eb7d9c5c218278bfcc83f476b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '2tvenom/cborencode' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => '42aedccb861d01fc0554782348cc08f8ebf22332',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../2tvenom/cborencode',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '0f39aa9d39525e7eb7d9c5c218278bfcc83f476b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'bordoni/phpass' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '0.3.6',
+            ),
+        ),
+        'composer/ca-bundle' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'coolsms/php-sdk' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.*',
+            ),
+        ),
+        'davidearl/webauthn' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '91d2011226193eadd383ff407b41d7e8261d43f4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../davidearl/webauthn',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'doctrine/lexer' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '1.*',
+            ),
+        ),
+        'egulias/email-validator' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '3.2.*',
+            ),
+        ),
+        'enshrined/svg-sanitize' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '0.*',
+            ),
+        ),
+        'ezyang/htmlpurifier' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '4.18.*',
+            ),
+        ),
+        'firebase/php-jwt' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '6.4.0',
+            ),
+        ),
+        'google/auth' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '1.26.*',
+            ),
+        ),
+        'guzzlehttp/guzzle' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '7.9.*',
+            ),
+        ),
+        'guzzlehttp/psr7' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.7.*',
+            ),
+        ),
+        'jbbcode/jbbcode' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '1.4.*',
+            ),
+        ),
+        'leafo/lessphp' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => 'dev-master',
+            ),
+        ),
+        'league/html-to-markdown' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '5.1.*',
+            ),
+        ),
+        'matthiasmullie/minify' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '1.3.*',
+            ),
+        ),
+        'matthiasmullie/path-converter' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '1.1.*',
+            ),
+        ),
+        'michelf/php-markdown' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '1.9.*',
+            ),
+        ),
+        'michelf/php-smartypants' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '1.8.*',
+            ),
+        ),
+        'paragonie/constant_time_encoding' => array(
+            'pretty_version' => 'v3.1.3',
+            'version' => '3.1.3.0',
+            'reference' => 'd5b01a39b3415c2cd581d3bd3a3575c1ebbd8e77',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../paragonie/constant_time_encoding',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'paragonie/random_compat' => array(
+            'pretty_version' => 'v9.99.100',
+            'version' => '9.99.100.0',
+            'reference' => '996434e5492cb4c3edcb9168db6fbb1359ef965a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../paragonie/random_compat',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpseclib/phpseclib' => array(
+            'pretty_version' => '3.0.49',
+            'version' => '3.0.49.0',
+            'reference' => '6233a1e12584754e6b5daa69fe1289b47775c1b9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpseclib/phpseclib',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/cache' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '1.0.1',
+            ),
+        ),
+        'rhymix/composer-stub' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'c7d4fcec98e99d9e5213c5fc96d35d406e6d0848',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../rhymix/composer-stub',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'rmccue/requests' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '1.8.*',
+            ),
+        ),
+        'scssphp/scssphp' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '1.13.*',
+            ),
+        ),
+        'swiftmailer/swiftmailer' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '6.3.0',
+            ),
+        ),
+        'symfony/deprecation-contracts' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.5.*',
+            ),
+        ),
+        'true/punycode' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.*',
+            ),
+        ),
+    ),
+);
---
+++ vendor/rhymix/composer-stub/README.md
@@ -0,0 +1,18 @@
+Composer Stub for Rhymix Third-Party Programs
+=============================================
+
+Start your module with the following `composer.json` and add any other library you need.
+Libraries included with Rhymix Core will be skipped
+so that you can avoid duplicates and version conflicts.
+
+```
+{
+  "config": {
+    "optimize-autoloader": true,
+    "prepend-autoloader": false
+  },
+  "require": {
+    "rhymix/composer-stub": "dev-master"
+  }
+}
+```
